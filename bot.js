5fb5ada741969d615c8e73bbfe796915d2e3602b
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-19 22:15:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'おはよう') {
+    	message.reply('おはようございます！');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
